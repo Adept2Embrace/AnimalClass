c3b240e65fe290a201b1eb667d7f227b391ea7f0
Angela Kaur <gurhar510@gmail.com>
2022-09-12 22:14:25 +1200

    Add project files.

---
+++ AnimalClass/Animal.cs
@@ -0,0 +1,33 @@
+﻿namespace AnimalClass
+{
+    public class Animal
+    {
+        
+        public int Age { get; set; }
+        
+        public Animal(int age)
+        {
+            this.Age = age;
+        }
+
+        public virtual bool isBaby()
+        {
+            return this.Age < 1;            
+        }
+    }
+
+    public class Dogs : Animal
+    {
+        public string Color { get; set; }
+
+        public Dogs(int age, string color) : base(age) // creating constructor for child class Dogs
+        {
+            this.Color = color;
+        }
+
+        public override bool isBaby()// overided function from parent class
+        {
+            return this.Age < 2;
+        }
+    }
+}
---
+++ AnimalClass/Program.cs
@@ -0,0 +1,38 @@
+﻿global using global::System;
+namespace AnimalClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Animal animal = new Animal(3);
+            Dogs dogs1 = new Dogs(0, "White");
+            Dogs dogs2 = new Dogs(0, "Grey");
+            Dogs dogs3 = new Dogs(1, "Black");
+            Dogs dogs4 = new Dogs(2, "Brown");
+            Dogs dogs5 = new Dogs(3, "Dark Grey");
+
+                     
+            //creating list of Dogs
+            List<Dogs> dogList = new List<Dogs>();
+            dogList.Add(dogs1);
+            dogList.Add(dogs2);
+            dogList.Add(dogs3);
+            dogList.Add(dogs4);
+            dogList.Add(dogs5);
+
+
+            //code to filter puppies
+            for (int i = 0; i < dogList.Count; i++)
+            {
+                if (dogList[i].isBaby())
+                {
+                    Console.WriteLine("{0} dog is a puppy with age {1}", dogList[i].Color, dogList[i].Age);
+                }
+
+            }          
+
+        }
+    }
+}
